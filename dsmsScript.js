b30a23ffd833249d78092f8f4f285d863085f9ed
Arun Dutta <adutta13@gmail.com>
2011-07-19 15:27:54 -0700

    another

--- dsmsScript.js
+++ dsmsScript.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
     var url ="http://2.desksms.appspot.com/api/v1/user/DSMS.clockwork@gmail.com/sms";
-    $.get("http://jsonp.deployfu.com/clean/" + encodeURIComponent(url),
+    $.get(url,
     function(data,textStatus,jqXHR)
     {
-        console.log(data);
+        console.log(jqXHR);
+                console.log(data);
         $('#content').append(data);
     }
     ,"jsonp").error(function(){
